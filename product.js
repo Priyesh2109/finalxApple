0d25d8746979b027dbe48822b93b7da9bef3e068
Priyesh patel <priyeshpatel9843516@icloud.com>
2020-08-17 15:35:17 +0100

    adding js and images

---
+++ product.js
@@ -0,0 +1,16 @@
+//selecting all slides
+const slides = document.querySelectorAll('.slide');
+
+// targeting the two buttons here
+const next = document.querySelector('#next')
+
+const prev =  document.querySelector('#prev')
+
+const auto = false;
+// 
+
+const intervalTime = 5000;
+// determines when to move to next slide
+let slideInterval;
+
+
