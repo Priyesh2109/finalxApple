aabc822685e59a775c1444036073de590689cdcb
Priyesh patel <priyeshpatel9843516@icloud.com>
2020-08-18 16:42:44 +0100

    slider images

--- product.js
+++ product.js
@@ -1,65 +1,61 @@
-//selecting all slides
 const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = false; // Auto scroll
+const intervalTime = 5000;
+let slideInterval;
 
-// targeting the two buttons here
-const next = document.querySelector('#next')
+const nextSlide = () => {
+  // Get current class
+  const current = document.querySelector('.current');
+  // Remove current class
+  current.classList.remove('current');
+  // Check for next slide
+  if (current.nextElementSibling) {
+    // Add current to next sibling
+    current.nextElementSibling.classList.add('current');
+  } else {
+    // Add current to start
+    slides[0].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
+};
 
-const prev =  document.querySelector('#prev')
+const prevSlide = () => {
+  // Get current class
+  const current = document.querySelector('.current');
+  // Remove current class
+  current.classList.remove('current');
+  // Check for prev slide
+  if (current.previousElementSibling) {
+    // Add current to prev sibling
+    current.previousElementSibling.classList.add('current');
+  } else {
+    // Add current to last
+    slides[slides.length - 1].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
+};
 
-const auto = false;
-// 
+// Button events
+next.addEventListener('click', e => {
+  nextSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
 
-    const intervalTime = 5000;
-// determines when to move to next slide
-    let slideInterval;
+prev.addEventListener('click', e => {
+  prevSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
 
-    const nextSlide = () =>{
-    // get the curent image
-    const current =document.querySelector('.current');
-
-    // i want to move onto the next image
-
-    current.classList.remove('.current');
-
-    // need to check if next image is present if there is add it else go back to the beggining
-         if(current.nextElementSibling){
-     current.nextElementSibling.classList.add('current')
-         }
-         else{
-             slides[0].classList.add(current);
-         }
-
-         setTimeout(()=> current.add.classList.remove('current'))
+// Auto slide
+if (auto) {
+  // Run next slide at interval time
+  slideInterval = setInterval(nextSlide, intervalTime);
 }
-
-
-// previous slide function-go back
-
-const previousSlide = () =>{
-    // get the curent image
-    const current =document.querySelector('.current');
-
-    // i want to move onto the next image
-
-    current.classList.remove('.current');
-
-    // need to check if previous image is present if there is add it else go back to the very last one
-         if(current.previousElementSibling){
-     current.previousElementSibling.classList.add('current')
-         }
-         else{
-             slides[slides.length-1].classList.add(current);
-         }
-
-         setTimeout(()=> current.add.classList.remove('current'))
-}
-
-
-// now to add event listener for the buttons
-next.addEventListener('click', e=>{
-    nextSlide();
-})
-
-prev.addEventListener('click', e=>{
-    prevSlide();
-})
